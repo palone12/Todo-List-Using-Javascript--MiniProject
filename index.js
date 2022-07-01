53a00c1f9a1203240173b3a73cbda9041f2ba830
Pranay Alone <palone12>
2022-07-01 15:50:58 +0530

    Added More JavaScript

--- index.js
+++ index.js
@@ -3,8 +3,24 @@ function addMore(){
     var li = document.createElement('li');
     var ul =document.getElementById("addHere");
     var text = document.createTextNode(input)
+    var delte= document.createElement('button');
+    delte.textContent="Delete"
+  
     li.appendChild(text)
+
+    li.appendChild(delte)
     ul.appendChild(li);
+    delte.style.backgroundColor="#FFF9B6";
+    delte.style.padding='10px';
+    delte.style.borderRadius='20px'
+    li.style.display='flex';
+    li.style.justifyContent='space-between';
+    li.style.alignItems='center'
+    li.style.padding="10px";
+    li.style.listStyle= "circle";  delte.addEventListener('click',()=>{
+        ul.removeChild(li);
+    })
+
     document.getElementById("taskItem").value="";
     
 
